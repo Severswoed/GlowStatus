cd48ff0032b64137b6dae163f32b7346e12dffd6
Chris Inman <severswoed@gmail.com>
2025-06-24 13:42:41 -0400

    Add window icon to configuration UI and refactor logger setup for single handler

--- src/logger.py
+++ src/logger.py
@@ -2,10 +2,15 @@ import logging
 
 def get_logger(name="GlowStatus"):
     logger = logging.getLogger(name)
-    if not logger.handlers:  # Only add handler if none exist for this logger
-        logger.setLevel(logging.INFO)
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter('[%(asctime)s] %(levelname)s: %(message)s')
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
+    logger.propagate = False  # Prevent double logging via parent/root logger
+
+    # Remove all handlers before adding one (guaranteed single handler)
+    while logger.handlers:
+        logger.removeHandler(logger.handlers[0])
+
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('[%(asctime)s] %(levelname)s: %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
     return logger
